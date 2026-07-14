e53c57d8ce17546ad4d220b161d331fcdc822884
suchan5 <suchan5@hotmail.com>
2026-07-14 21:13:47 +0800

    feat: implement CustomerService create customer logic

---
+++ src/main/java/com/suchan/banking/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.suchan.banking.service;
+
+import com.suchan.banking.dto.CreateCustomerRequest;
+import com.suchan.banking.dto.CreateCustomerResponse;
+import com.suchan.banking.entity.Customer;
+import com.suchan.banking.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+
+    private final CustomerRepository customerRepository; // 나중에 다른 Repository로 바뀌면 안 되니까 final을 붙이는 거야.
+
+    // 이게 생성자 주입(Constructor Injection) 이야.
+    public CustomerService(CustomerRepository customerRepository) { // Spring이 실행될 때 "CustomerRepository 만들어놨으니까 CustomerService에 넣어줄게."
+        this.customerRepository = customerRepository;               // 그래서 Service 안에서 customerRepository.save(...) 를 사용할 수 있는 거야.
+    }
+
+    //고객 생성 결과를 클라이언트에게 돌려줘야 하니까 반환 타입은 CreateCustomerResponse
+    public CreateCustomerResponse createCustomer(CreateCustomerRequest request) {
+        Customer customer = Customer.builder() //클라이언트가 보낸 request에서 name, email을 꺼내 Customer Entity를 만들어.
+                .name(request.getName())       // id는 db가 만들어주니까 넣지 않는다
+                .email(request.getEmail())
+                .build();
+
+        Customer savedCustomer = customerRepository.save(customer); // Repository의 save()를 호출하면 저장된 Customer Entity가 반환돼.
+
+        //그다음 저장된 Entity를 Response DTO로 변환해 반환해.
+        return CreateCustomerResponse.builder()
+                .id(savedCustomer.getId())
+                .name(savedCustomer.getName())
+                .email(savedCustomer.getEmail())
+                .build();
+
+    }
+}
