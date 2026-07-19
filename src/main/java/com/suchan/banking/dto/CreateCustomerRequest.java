832e1591b5c5b9d3af741b0e4f2084dee070bf0d
suchan5 <suchan5@hotmail.com>
2026-07-19 12:33:00 +0800

    feat: add size validation for customer name

--- src/main/java/com/suchan/banking/dto/CreateCustomerRequest.java
+++ src/main/java/com/suchan/banking/dto/CreateCustomerRequest.java
@@ -2,6 +2,7 @@ package com.suchan.banking.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -11,6 +12,7 @@ import lombok.*;
 @Builder
 public class CreateCustomerRequest {
     @NotBlank(message = "Name is required")
+    @Size(min=2, max=50, message = "Name must be between 2 and 50 characters")
     private String name;
 
     @NotBlank(message = "Email is required")
